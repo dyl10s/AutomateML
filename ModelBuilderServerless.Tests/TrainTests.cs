ba645909e4de020ae0de4106bf9f0ecd9a07b42c
Dylan Strohschein <dylanstrohschein@gmail.com>
2020-04-19 02:50:36 -0400

    Added test for serverless function

---
+++ ModelBuilderServerless.Tests/TrainTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelBuilderServerless;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelBuilderServerless.Tests
+{
+    [TestClass()]
+    public class TrainTests
+    {
+        [TestMethod()]
+        public void RunTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
